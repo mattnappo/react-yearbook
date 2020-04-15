70f37454efc7515b539d2a9be87ac083f8a793b6
xoreo <mattnappo@gmail.com>
2020-04-15 12:11:39 -0400

    Clean up state usage everywhere.

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'universal-cookie';
-import { TextField, makeStyles } from '@material-ui/core';
+import { TextField, Button, makeStyles } from '@material-ui/core';
 import { apiEndpoint } from './utils';
 
 const useStyles = makeStyles((theme) => ({
@@ -18,6 +18,11 @@ const Settings = (props) => {
 
   // Just to display data initially
   const [userData, setUserData] = useState('');
+  const updateUserData = (e, field) => {
+    const newUserData = userData;
+    newUserData[field] = e.target.value;
+    setUserData(newUserData);
+  };
 
   const getUserData = () => {
     const { username } = props.match.params;
@@ -42,6 +47,36 @@ const Settings = (props) => {
       });
   };
 
+  const save = () => {
+    fetch(
+      apiEndpoint('updateUser'),
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `bearer ${cookies.get('token')}`,
+        },
+        body: {
+          username: userData.username,
+          grade: userData.grade,
+          bio: userData.bio,
+          will: userData.will,
+          profile_pic: userData.profile_pic,
+          nickname: userData.nickname,
+        },
+      },
+    ).then((res) => res.json())
+      .then((res) => {
+        if (res.errors) {
+          // eslint-disable-next-line no-console
+          console.log(res);
+          // window.location.href = '/';
+        }
+
+        console.log('Changes were saved');
+      });
+  };
+
   const genSeniorWill = () => {
     if (userData.grade === 'senior') {
       return (
@@ -51,12 +86,7 @@ const Settings = (props) => {
           multiline
           rowsMax={4}
           value={userData.will}
-          onChange={(e) => {
-            const newUserData = userData;
-            newUserData.will = e.target.value;
-            console.log(`NEW USER DATA ${JSON.stringify(newUserData)}`);
-            setUserData(newUserData);
-          }}
+          onChange={(e) => { updateUserData(e, 'will'); }}
           variant="outlined"
         />
       );
@@ -68,17 +98,27 @@ const Settings = (props) => {
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="standard-basic" label="Nickname" />
-      <TextField id="standard-basic" label="Grade" />
+      <TextField
+        id="standard-basic"
+        label="Nickname"
+        value={userData.nickname}
+        onChange={(e) => { updateUserData(e, 'nickname'); }}
+      />
+      <TextField
+        id="standard-basic"
+        label="Grade"
+        value={userData.grade}
+        onChange={(e) => { updateUserData(e, 'grade'); }}
+      />
 
       <div>
         <TextField
           id="outlined-multiline-flexible"
-          label="Bio"
+          label="Senior Will"
           multiline
           rowsMax={4}
-          value={bio}
-          onChange={(e) => { setBio(e.target.value); }}
+          value={userData.will}
+          onChange={(e) => { updateUserData(e, 'will'); }}
           variant="outlined"
         />
 
@@ -88,6 +128,11 @@ const Settings = (props) => {
       <div>
         <TextField id="standard-basic" label="Standard" />
       </div>
+
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+
     </form>
   );
 };
