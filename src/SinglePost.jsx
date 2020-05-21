46e60b418b2cc59082e4b8104a242b687f40b2a0
xoreo <mattnappo@gmail.com>
2020-05-21 12:53:20 -0400

    Really good place right now.

--- src/SinglePost.jsx
+++ src/SinglePost.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
 import Post from './Post';
 import TopBar, { BottomBar } from './Bar';
 
@@ -6,6 +7,7 @@ const SinglePost = (props) => (
   <div>
     <TopBar />
     <div className="main-content">
+      <Box className="header">Post</Box>
       <Post postData={props.location.state.post} single />
     </div>
     <BottomBar />
